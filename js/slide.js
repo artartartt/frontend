fe3f7721d831143f834eaf71215d5bd157765572
Artyom <hovasapyan_artyom03@mail.ru>
2021-11-09 19:03:40 +0400

    Add files via upload

---
+++ js/slide.js
@@ -0,0 +1,11 @@
+$(document).ready(function (){
+	$('.slider').slick({
+		arrows:false,
+		dots:true,
+		slidesToShow: 6,
+		slidesToScroll: 5,
+	});
+});
+
+
+
---
+++ js/slide_my_works.js
@@ -0,0 +1,21 @@
+$(document).ready(function (){
+	$('.slider_my_works').slick({
+		arrows:true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+	});
+});
+
+
+function next_img(){
+	$(document).ready(function(){
+		$('.slider_my_works .slick-next').click();
+	});
+}
+function previous_img(){
+	$(document).ready(function(){
+		$('.slider_my_works .slick-prev').click();
+	});
+}
+
+
