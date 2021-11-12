c3ff92cffa145974f8bcfd4794745f14607c1b6a
Artyom <hovasapyan_artyom03@mail.ru>
2021-11-12 12:34:01 +0400

    Add files via upload

--- js/slide.js
+++ js/slide.js
@@ -1,4 +1,10 @@
+
 $(document).ready(function (){
+	$('.percentage_html_1_hr').addClass('html_1_hr_animation');
+	$('.percentage_html_2_hr').addClass('html_2_hr_animation');
+	$('.percentage_html_3_hr').addClass('html_3_hr_animation');
+	$('.percentage_html_4_hr').addClass('html_4_hr_animation');
+
 	$('.slider').slick({
 		arrows:false,
 		dots:true,
